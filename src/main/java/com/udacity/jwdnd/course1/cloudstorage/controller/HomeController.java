2e6b734c3f39d61b7c66968832261f9e5587a3d7
Paul Reiners <paul.reiners@gmail.com>
2020-08-06 16:04:46 -0500

    Get user name.

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.udacity.jwdnd.course1.cloudstorage.controller;
 
 import com.udacity.jwdnd.course1.cloudstorage.model.FileForm;
 import com.udacity.jwdnd.course1.cloudstorage.services.FileService;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,8 +28,9 @@ public class HomeController {
     }
 
     @PostMapping("/home")
-    public String newFile(FileForm newFile, Model model) throws IOException {
-        fileService.addFile(newFile.getFile());
+    public String newFile(Authentication authentication, FileForm newFile, Model model) throws IOException {
+        String userName = authentication.getName();
+        fileService.addFile(newFile.getFile(), userName);
         model.addAttribute("files", fileService.getFileListings());
 
         return "home";
--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/FileService.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/FileService.java
@@ -1,35 +1,45 @@
 package com.udacity.jwdnd.course1.cloudstorage.services;
 
 import com.udacity.jwdnd.course1.cloudstorage.mapper.FileMapper;
+import com.udacity.jwdnd.course1.cloudstorage.mapper.UserMapper;
 import com.udacity.jwdnd.course1.cloudstorage.model.File;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 @Service
 public class FileService {
     private final FileMapper fileMapper;
+    private final UserMapper userMapper;
 
-    public FileService(FileMapper fileMapper) {
+    public FileService(FileMapper fileMapper, UserMapper userMapper) {
         this.fileMapper = fileMapper;
+        this.userMapper = userMapper;
     }
 
     public String[] getFileListings() {
         return fileMapper.getFileListings();
     }
 
-    public void addFile(MultipartFile multipartFile) throws IOException {
-        int fileId = 0;
+    public void addFile(MultipartFile multipartFile, String userName) throws IOException {
         InputStream fis = multipartFile.getInputStream();
-        String fileName = multipartFile.getName();
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        int nRead;
+        byte[] data = new byte[1024];
+        while ((nRead = fis.read(data, 0, data.length)) != -1) {
+            buffer.write(data, 0, nRead);
+        }
+        buffer.flush();
+        byte[] fileData = buffer.toByteArray();
+
+        String fileName = multipartFile.getOriginalFilename();
         String contentType = multipartFile.getContentType();
         String fileSize = String.valueOf(multipartFile.getSize());
-        Integer userId = 0;
-        byte[] fileData = multipartFile.getBytes();
-        File file = new File(fileId, fileName, contentType, fileSize, userId, fileData);
-        fileId = fileMapper.insert(file);
-        System.out.println(fileId);
+        Integer userId = userMapper.getUser(userName).getUserId();
+        File file = new File(0, fileName, contentType, fileSize, userId, fileData);
+        fileMapper.insert(file);
     }
 }
