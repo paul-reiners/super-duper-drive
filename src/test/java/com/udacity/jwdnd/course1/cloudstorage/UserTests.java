36046d65e11dd7f00906f32b226ecfae7ae8a66e
Paul Reiners <paul.reiners@gmail.com>
2020-08-12 13:12:16 -0500

    Not sure.

--- src/test/java/com/udacity/jwdnd/course1/cloudstorage/UserTests.java
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/UserTests.java
@@ -2,11 +2,8 @@ package com.udacity.jwdnd.course1.cloudstorage;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
